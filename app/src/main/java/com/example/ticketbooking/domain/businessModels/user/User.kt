bc3bedd8d191b0b0ff841ca9a75064170bf774d8
dharan-pt6340 <dharanendhiranjain@gmail.com>
2023-02-01 17:17:46 +0530

    .

--- app/src/main/java/com/example/ticketbooking/domain/businessModels/user/User.kt
+++ app/src/main/java/com/example/ticketbooking/domain/businessModels/user/User.kt
@@ -4,9 +4,7 @@ data class User(
     var name: String,
     var phoneNo: String,
     var mailId: String,
-    var profilePicture: String?,
-    var userName: String,
-    var password: String,
+    var profilePicture: String?,//no need username and password here
     var offers: MutableList<Offer>,
     var orders: MutableList<Order>
 )
