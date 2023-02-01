a822ff66e9edc9fc73d5f47cbc055dbdc2a5ab91
dharan-pt6340 <dharanendhiranjain@gmail.com>
2023-02-01 15:11:22 +0530

    .

--- app/src/main/java/com/example/ticketbooking/domain/businessModels/theatre/Address.kt
+++
@@ -1,8 +0,0 @@
-package com.example.ticketbooking.domain.businessModels.theatre
-
-data class Address(
-    var doorNo: String,
-    var street: String,
-    var area: String,
-    var city: String
-)
