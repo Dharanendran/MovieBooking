f29bb368517c9bfe78e8381881808e682c9acafe
dharan-pt6340 <dharanendhiranjain@gmail.com>
2023-02-14 11:47:59 +0530

    .

--- app/src/main/java/com/example/ticketbooking/dataRepository/roomDatabase/entities/FavouriteMovies.kt
+++ app/src/main/java/com/example/ticketbooking/dataRepository/roomDatabase/entities/FavouriteMovies.kt
@@ -1,10 +1,31 @@
 package com.example.ticketbooking.dataRepository.roomDatabase.entities
 
 import androidx.room.Entity
+import androidx.room.ForeignKey
+import androidx.room.PrimaryKey
 
 
 @Entity(
-    tableName = "favourite_movies_table"
+    tableName = "favourite_movies_table",
+    foreignKeys = [
+        ForeignKey(
+            entity = User::class,
+            parentColumns = ["id"],
+            childColumns = ["userId"],
+            onDelete = ForeignKey.CASCADE ),
+        ForeignKey(
+            entity = Movie::class,
+            parentColumns = ["id"],
+            childColumns = ["movieId"],
+            onDelete = ForeignKey.CASCADE
+        )
+    ]
 )
-class FavouriteMovies {
+data class FavouriteMovies(
+    var userId:Long,
+    var movieId:Long
+)
+{
+    @PrimaryKey(autoGenerate = true)
+    var id:Long = 0
 }
